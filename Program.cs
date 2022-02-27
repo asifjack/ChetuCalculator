37b6612e272e3a6f39d322dc9cc60f41b678566c
asifjack <mdasifwr@gmail.com>
2022-02-27 22:57:01 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ChetuCalculator
+{
+    public class Cal
+    {
+        public void Add(decimal num1,decimal num2)
+        {
+            Console.WriteLine("Sum : "+(num1 + num2));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Numbers ..");
+            decimal num1 =Decimal.Parse( Console.ReadLine());
+            decimal num2 = Decimal.Parse(Console.ReadLine());
+            Cal cal = new Cal();
+            cal.Add(num1, num2);
+            
+
+        }
+    }
+}
