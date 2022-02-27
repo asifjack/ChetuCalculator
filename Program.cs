c9dca3c300ff2268bbb5717427c6ba5836d2d6fd
asifjack <mdasifwr@gmail.com>
2022-02-27 23:07:26 +0530

    Add function

--- Program.cs
+++ Program.cs
@@ -8,18 +8,47 @@ namespace ChetuCalculator
         {
             Console.WriteLine("Sum : "+(num1 + num2));
         }
+        public void Sub(decimal num1, decimal num2)
+        {
+            Console.WriteLine("Sub : " + (num1 - num2));
+        }
+        public void Mult(decimal num1, decimal num2)
+        {
+            Console.WriteLine("Mult : " + (num1*num2));
+        }
+        public void Div(decimal num1, decimal num2)
+        {
+            Console.WriteLine("Div : " + (num1/num2));
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
+            Cal cal = new Cal();
             Console.WriteLine("Enter Numbers ..");
             decimal num1 =Decimal.Parse( Console.ReadLine());
             decimal num2 = Decimal.Parse(Console.ReadLine());
-            Cal cal = new Cal();
-            cal.Add(num1, num2);
-            
-
+            Console.WriteLine("Enter operation ..");
+            string opp = Console.ReadLine();
+            switch (opp)
+            {
+                case "add":
+                    cal.Add(num1, num2);
+                    break;
+                case "sub":
+                    cal.Sub(num1, num2);
+                    break;
+                case "mult":
+                    cal.Mult(num1, num2);
+                    break;
+                case "di":
+                    cal.Div(num1, num2);
+                    break;
+                default:
+                    Console.WriteLine("Looking For Valid Operation");
+                    break;
+            }
         }
     }
 }
